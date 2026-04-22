719ec7629b740c716ad34978865f0b2493f756e7
KissEmAll <kissemall.ent@proton.me>
2026-04-22 13:38:38 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.externals.push('@runwayml/avatars-node-rpc')
+    }
+    return config
+  },
 }
 module.exports = nextConfig
